11c4a8197edb60497183b14115c6644e2c528512
pinxuan <daipinxuan@zju.edu.cn>
2025-05-20 15:33:53 +0800

    update

--- src/drc_reader/__init__.py
+++ src/drc_reader/__init__.py
@@ -2,9 +2,12 @@ from drc_decoder import drc2ply
 
 
 
-def read_drc_as_ply(path):
+def drc_file_to_ply(path):
     with open(path, 'rb') as file:
         drc = file.read()
+    return drc_bytes_to_ply(drc)
+
+def drc_bytes_to_ply(drc):
     ply = drc2ply(drc)
     # print(len(drc), len(ply))
     return ply
